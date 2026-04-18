aae82b302a74d470dbc67520a380842aac5ae641
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-04-18 15:00:59 +0200

    Add initial Terraform configuration and setup files

---
+++ infra/0-bootstrap/main.tf
@@ -0,0 +1,38 @@
+# Configuration
+
+terraform {
+  required_version = ">= 1.14"
+
+  backend "gcs" {
+    bucket = "ms-tfstate-f1cbea4f18df7b19"
+    prefix = "projects/counter/v2/bootstrap"
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.11"
+    }
+  }
+}
+
+# Module imports
+
+module "common" {
+  source = "../modules/common"
+}
+
+# Providers
+
+# GitHub provider for accessing CI/CD variables
+
+variable "gh_token" {
+  description = "Organization-owned GitHub token."
+  type        = string
+  sensitive   = true
+}
+
+provider "github" {
+  owner = module.common.gh_organization_name
+  token = var.gh_token
+}
