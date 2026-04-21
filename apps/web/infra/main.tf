bf4d887677f71528572e1f6c5a8fc7e8fade6177
Jakub Trzebiatowski <jakub.trzebiatowski@medusa.software>
2026-04-21 15:53:01 +0200

    Update Terraform prefixes

--- apps/web/infra/main.tf
+++ apps/web/infra/main.tf
@@ -5,7 +5,7 @@ terraform {
 
   backend "gcs" {
     bucket = "ms-tfstate-c1984596bdabf023"
-    prefix = "projects/counter/v2/counter-web/foundation" # 🎨 TEMPLATE EJECT: Update the prefix (!)
+    prefix = "projects/counter/v2/apps/web/foundation" # 🎨 TEMPLATE EJECT: Update the prefix (!)
   }
 
   required_providers {
